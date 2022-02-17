2ac0580dafd77c2380318955da37944b45715a5c
ashish-ad <ashishad14@gmail.com>
2022-02-17 17:04:47 +0530

    abstraction code for bootcamp

--- BootCamp-Projects/abstraction.py
+++ BootCamp-Projects/abstraction.py
@@ -1,27 +1,27 @@
-# from abc import abstractmethod, ABC
+from abc import abstractmethod, ABC
 
 
-# class atm(ABC):
+class atm(ABC):
 
-#     @abstractmethod
-#     def withdraw_money(self):
-#         pass
+    @abstractmethod
+    def withdraw_money(self):
+        pass
 
-# class hdfc(atm):
-#     def withdraw_money():
-#         print((input("Input money u require: ")))
+class hdfc(atm):
+    def withdraw_money():
+        print((input("Input money u require: ")))
 
-# class canara(atm):
-#     def withdraw_money():
-#         print((input("Input money u require: ")))
+class canara(atm):
+    def withdraw_money():
+        print((input("Input money u require: ")))
 
-# class sbi(atm):
-#     def withdraw_money():
-#         print((input("Input money u require: ")))
+class sbi(atm):
+    def withdraw_money():
+        print((input("Input money u require: ")))
 
 
-# money_withdraw = sbi()
-# sbi.withdraw_money()
+money_withdraw = sbi()
+sbi.withdraw_money()
 
 
 
